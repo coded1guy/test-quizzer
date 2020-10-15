4297013c1440fdb2429ecd1ab6fd9a0ac794bb5c
Olubiyi Emmanuel <42422184+coded1guy@users.noreply.github.com>
2020-10-15 14:31:40 +0100

    Update mode.js

--- js/mode.js
+++ js/mode.js
@@ -1,5 +1,33 @@
 let intMode;
 
+// FOR THE MODE.HTML FILE
+{
+    let radOne = document.querySelector('#rad1');
+    let radTwo = document.querySelector('#rad2');
+    let inputOne = document.querySelector('#ip1');
+    let inputTwo = document.querySelector('#ip2');
+    let loadBtn = document.querySelector('#load');
+    let closeBtn = document.querySelector('#close');
+
+// {
+//     inputTwo.value = " ";
+//     console.log(inputTwo.value);
+//     inputTwo.oninput = () => {
+//         console.log(inputTwo.value);
+//     }
+// }
+
+    loadBtn.onclick = () => {
+        window.location.assign('quiz.html');
+    };
+    closeBtn.onclick = () => {
+        window.location.assign('mode-landing-pg.html');
+    };
+}
+
+
+
+
 // FOR THE MODE-LANDING-BG.HTML PAGE
 {
     let modeSet1 = document.querySelector('.time-based-game');
@@ -27,30 +55,7 @@ let intMode;
 }
 
 
-// FOR THE MODE.HTML FILE
-{
-    let radOne = document.querySelector('#rad1');
-    let radTwo = document.querySelector('#rad2');
-    let inputOne = document.querySelector('#ip1');
-    let inputTwo = document.querySelector('#ip2');
-    let loadBtn = document.querySelector('#load');
-    let closeBtn = document.querySelector('#close');
 
-// {
-//     inputTwo.value = " ";
-//     console.log(inputTwo.value);
-//     inputTwo.oninput = () => {
-//         console.log(inputTwo.value);
-//     }
-// }
-
-    loadBtn.onclick = () => {
-        window.location.assign('quiz.html');
-    };
-    closeBtn.onclick = () => {
-        window.location.assign('mode-landing-pg.html');
-    };
-}
 
 
 // PARAMETERS THAT WILL BE GOTTEN
