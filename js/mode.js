a0d73a480fe0c42a0063c177a35a8c47c435a20a
Olubiyi Emmanuel <42422184+coded1guy@users.noreply.github.com>
2020-10-15 16:42:14 +0100

    Update mode.js

--- js/mode.js
+++ js/mode.js
@@ -1,60 +1,25 @@
-let intMode;
+let intMode = localStorage.getItem("mode");
 
 // FOR THE MODE.HTML FILE
+let radOne = document.querySelector('#rad1');
+let radTwo = document.querySelector('#rad2');
+let inputOne = document.querySelector('#ip1');
+let inputTwo = document.querySelector('#ip2');
+let loadBtn = document.querySelector('#load');
+let closeBtn = document.querySelector('#close');
 
-    let radOne = document.querySelector('#rad1');
-    let radTwo = document.querySelector('#rad2');
-    let inputOne = document.querySelector('#ip1');
-    let inputTwo = document.querySelector('#ip2');
-    let loadBtn = document.querySelector('#load');
-    let closeBtn = document.querySelector('#close');
-
-
-       inputTwo.value = " ";
-       console.log(inputTwo.value);
-       inputTwo.oninput = () => {
-           console.log(inputTwo.value);
-       }
-
-
-    loadBtn.onclick = () => {
-        window.location.assign('quiz.html');
-    };
-    closeBtn.onclick = () => {
-        window.location.assign('mode-landing-pg.html');
-    };
-
-
-
-
-
-// FOR THE MODE-LANDING-BG.HTML PAGE
-{
-    let modeSet1 = document.querySelector('.time-based-game');
-    let modeSet2 = document.querySelector('.no-based-game');
-
-
-    let goToTheProperty = (value) => {
-        window.location.assign('mode.html');
-        console.log('start');
-        if(value === "time based Game") {
-             console.log("1 is true");
-        } else if(value === "question-no based Game") {
-            console.log("2 is true");
-        }
-        intMode = value;
-        console.log('sop');
-    }
-
-    modeSet1.onclick = () => {
-         goToTheProperty("time based Game");
-    };
-    modeSet2.onclick = () => {
-         goToTheProperty("question-no based Game");
-    };
+inputTwo.value = " ";
+console.log(inputTwo.value);
+inputTwo.oninput = () => {
+    console.log(inputTwo.value);
 }
-
-
+    
+loadBtn.onclick = () => {
+    window.location.assign('quiz.html');
+};
+closeBtn.onclick = () => {
+    window.location.assign('mode-landing-pg.html');
+};
 
 
 
