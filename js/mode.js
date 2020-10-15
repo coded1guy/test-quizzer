c668a47321eceaefc9a7dae86efa312eeac5fbf0
Olubiyi Emmanuel <42422184+coded1guy@users.noreply.github.com>
2020-10-15 14:34:59 +0100

    Update mode.js

--- js/mode.js
+++ js/mode.js
@@ -1,7 +1,7 @@
 let intMode;
 
 // FOR THE MODE.HTML FILE
-{
+
     let radOne = document.querySelector('#rad1');
     let radTwo = document.querySelector('#rad2');
     let inputOne = document.querySelector('#ip1');
@@ -9,13 +9,13 @@ let intMode;
     let loadBtn = document.querySelector('#load');
     let closeBtn = document.querySelector('#close');
 
-// {
-//     inputTwo.value = " ";
-//     console.log(inputTwo.value);
-//     inputTwo.oninput = () => {
-//         console.log(inputTwo.value);
-//     }
-// }
+
+       inputTwo.value = " ";
+       console.log(inputTwo.value);
+       inputTwo.oninput = () => {
+           console.log(inputTwo.value);
+       }
+
 
     loadBtn.onclick = () => {
         window.location.assign('quiz.html');
@@ -23,7 +23,7 @@ let intMode;
     closeBtn.onclick = () => {
         window.location.assign('mode-landing-pg.html');
     };
-}
+
 
 
 
